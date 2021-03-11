c5985c446133954bb1a0c5501cdc629f420c8680
Ingo Muschenetz <ingo@muschenetz.com>
2021-03-11 15:14:23 -0800

    Fixed errors and pared down permissions again

--- popup.js
+++ popup.js
@@ -1,11 +1,14 @@
 // Inject the payload.js script into the current tab after the popout has loaded
 window.addEventListener('load', function (evt) {
-	chrome.extension.getBackgroundPage().chrome.tabs.executeScript(null, {
+	chrome.tabs.executeScript(null, {
 		file: 'payload.js'
-	});;
+	}, _=>chrome.runtime.lastError);
 });
 
 // Listen to messages from the payload.js script and write to popout.html
 chrome.runtime.onMessage.addListener(function (message) {
-	document.getElementById('chat').innerHTML = message;
+	var chat = document.getElementById('chat');
+	if(chat) {
+		chat.innerHTML = message;
+	}
 });
