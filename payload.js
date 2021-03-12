1abffa6b414a441de8ccf311eaa03d9583c6e786
Ingo Muschenetz <ingo@muschenetz.com>
2021-03-11 16:15:16 -0800

    Added support for team chats

--- payload.js
+++ payload.js
@@ -2,22 +2,59 @@
 chrome.runtime.sendMessage(sendChat());
 
 function sendChat() {
-  var messages = document.getElementsByClassName("message-body");
-  if (messages.length == 0) {
+
+  var linksText = "";
+
+  var threads = document.getElementsByClassName("ts-message-list-item");
+  if (threads.length == 0) {
     window.alert("This does not appear to be a Microsoft Teams Chat window");
     return null;
   }
 
-  var linksText = "";
+  for (let i = 0; i < threads.length; i++) {
+    var thread = threads[i];
 
-  for (let index = 0; index < messages.length; index++) {
+    var messages = thread.querySelectorAll("div.ts-message-thread-body");
+    var hidden = thread.querySelector('.ts-collapsed-string > div[aria-hidden="true"]');
+    // only display threaded view if in team chat
+    if(messages.length > 1)
+    {
+      linksText += "<div class='threaded'>"
+    }
 
-    var message = messages[index];
-    var name = message.querySelector('.ts-msg-name');
-    var timestamp = message.querySelector('.message-datetime');
-    var content = message.querySelector('.message-body-content');
+    for (let index = 0; index < messages.length; index++) {
 
-    linksText += "<p><b>" + name.innerText + "</b> " + timestamp.innerText + "<br>" + content.innerText + "</p>";
+      var message = messages[index];
+      var name = message.querySelector('div.ts-msg-name');
+      var timestamp = message.querySelector('span.message-datetime');
+      var content = message.querySelector('div.message-body-content');
+
+
+      var reply = "";
+      if(index > 0) {
+        reply = " class='reply' "
+      }
+      linksText += "<p" + reply + ">"
+      if(name) {
+        linksText += "<b>" + name.innerText + "</b> ";
+      }
+      if(timestamp) {
+        linksText += timestamp.innerText;
+      }
+      if(content) {
+        linksText += "<br>" + content.innerText;
+      }
+      if(hidden && hidden.innerText != "Collapse all") {
+        linksText += "<br><div class='collapsed'>" + hidden.innerText + "</div>";
+      }
+      linksText += "</p>"
+    }
+
+    // only display threaded view if in team chat
+    if(messages.length > 1)
+    {
+      linksText += "</div>"
+    }
   }
 
   return linksText;
